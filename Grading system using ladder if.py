8a0b5cda74bc67724c0688979e7ae4987030067c
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2022-09-25 23:27:19 +0530

    Add files via upload

---
+++ Grading system using ladder if.py
@@ -0,0 +1,12 @@
+m=int(input("Enter full marks out of 500="))
+if m>=300 and m<=500:
+    print(" conguralation you got: \n'First Division:'")
+elif m>=225 and m<300:
+      print(" conguralation you got: \n'Second Division:'")
+elif m>=150 and m<225:
+      print(" conguralation you got: \n'Third Division:'")
+elif m>=0 and m<150:
+      print(" Sorry dear: \n'Fail:'")
+else:
+    print("invalid")
+
